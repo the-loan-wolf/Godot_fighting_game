d516928684dc59ff2ef3f9aff2b201955982771a
rishav <rishavb518@gmail.com>
2026-08-19 23:13:30 +0530

    code cleanup

--- enemy_knight.gd
+++ enemy_knight.gd
@@ -3,119 +3,97 @@ extends CharacterBody2D
 var player = null
 var Attack = false
 var health = 100
-var hud
 var death = false 
 var attackList = ["Attack1", "Attack2", "Attack3"]
 var current_attack
 var current_collisionShape
 var current_hitbox
 
+@onready var HUD = $"../HUD"
+@onready var Attack1_hitbox_CollisionShape = $Attack1_hitbox/CollisionShape2D
+@onready var Attack1_hitbox = $Attack1_hitbox
+@onready var Attack2_hitbox_CollisionShape = $Attack2_hitbox/CollisionShape2D
+@onready var Attack2_hitbox = $Attack2_hitbox
+@onready var Attack3_hitbox_CollisionShape = $Attack3_hitbox/CollisionShape2D
+@onready var Attack3_hitbox = $Attack3_hitbox
+@onready var Run_attack_hitbox_CollisionShape = $Run_Attack_hitbox/CollisionShape2D
+@onready var Run_attack_hitbox = $Run_Attack_hitbox
+@onready var sprite = $AnimatedSprite2D
+@onready var PlayerDetectionarea_CollisionShape = $PlayerDetectionarea/CollisionShape2D
+@onready var hurtbox_CollisionShape = $hurtbox/CollisionShape2D
+@onready var CollisionShape = $CollisionShape2D
+
 func _ready() -> void:
 	#player = null
-	hud = $"../HUD"
-	$Attack1_hitbox/CollisionShape2D.set_deferred("disabled", true)
-	$Attack2_hitbox/CollisionShape2D.set_deferred("disabled", true)
-	$Attack3_hitbox/CollisionShape2D.set_deferred("disabled", true)
-	$Run_Attack_hitbox/CollisionShape2D.set_deferred("disabled", true)
+	Attack1_hitbox_CollisionShape.set_deferred("disabled", true)
+	Attack2_hitbox_CollisionShape.set_deferred("disabled", true)
+	Attack3_hitbox_CollisionShape.set_deferred("disabled", true)
+	Run_attack_hitbox_CollisionShape.set_deferred("disabled", true)
 
 func _physics_process(delta):
+	# gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		#var distance = global_position.distance_to(player.global_position)
-			#print(distance)
-	#if player == null:
-		#return
 	
 	if not Attack:
-		#$Attack2_hitbox/CollisionShape2D.set_deferred("disabled", true)
 		if current_collisionShape:
 			current_collisionShape.set_deferred("disabled", true)
 #
 	if not death:
-		#print(player)
 		if player and player.health > 0:
-			#print(player.got_hit)
 			var dis = player.global_position
 			var distance = dis.x
 			var diff = abs(dis.x - position.x)
-			#print(player.global_position)
-			#print("player: ",distance)
-			#print("Enemy: ",position)
 			if position.x > player.global_position.x:
-				if not $AnimatedSprite2D.flip_h:
-					$AnimatedSprite2D.flip_h = true
-					
+				if not sprite.flip_h:
+					sprite.flip_h = true
 			else:
-				$AnimatedSprite2D.flip_h = false
-		#
+				sprite.flip_h = false
+			#Attack state
 			if diff < 160 and !Attack:
-				#print("Attack2"
-				#print("1st block",distance)
 				velocity.x = 0
-				#Attack = true
 				start_attack()
-				#$hitBox/CollisionShape2D.set_deferred("disabled", false)
-				#$Attack2_sound.play()
-				if $AnimatedSprite2D.flip_h:
-					#print(current_collisionShape.position.x)
+				if sprite.flip_h:
 					if current_hitbox.position.x > 0:
 						current_hitbox.position.x *= -1
-				elif not $AnimatedSprite2D.flip_h:
+				elif not sprite.flip_h:
 					if current_hitbox.position.x < 0:
 						current_hitbox.position.x *= -1
+			#stand state
 			elif diff < 250:
-				#print("Stand Ready")
-				#print("2nd block",distance)
 				velocity.x = 0
-				#Attack = false
+			#walk state
 			elif diff < 600:
-				#print("last block", distance)
-				#print("Walk to Player")
-				#Attack = false
 				if position.x > player.global_position.x:
 					velocity.x = -150
-					pass
 				else:
 					velocity.x = 150
-					pass
 			else:
 				pass
-				#Attack = false
 			
 		move_and_slide()
 		
 		if is_on_floor():
 			if Attack:
 				pass
-				#if $AnimatedSprite2D.animation != "Attack2":
-				#print("Attack2 is: ", Attack2)
-				#if !$Attack2_sound.playing:
-					#$Attack2_sound.play()
-				#random_attack = attackList.pick_random()
-				#$AnimatedSprite2D.play(current_attack)
-				#if $AnimatedSprite2D.frame == 2:
-				#$random_attack/CollisionShape2D.set_deferred("disabled", false)
-				#get_node(current_attack + "_hitbox").get_node("CollisionShape2D").set_deferred("disabled", false)
 			else:
 				if velocity.x != 0:
-					$AnimatedSprite2D.play("Walk")
+					sprite.play("Walk")
 				else:
-					$AnimatedSprite2D.play("Idle")
-	
-
+					sprite.play("Idle")
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if $AnimatedSprite2D.animation.begins_with("Attack"):
+	if sprite.animation.begins_with("Attack"):
 		Attack = false
 		if player.got_hit:
-			if $AnimatedSprite2D.animation == "Attack1" :
+			if sprite.animation == "Attack1" :
 				player.health -= 1
-			elif $AnimatedSprite2D.animation == "Attack2" :
+			elif sprite.animation == "Attack2" :
 				player.health -= 2
-			elif $AnimatedSprite2D.animation == "Attack3" :
+			elif sprite.animation == "Attack3" :
 				player.health -= 3
-			#$AnimatedSprite2D.play("Hurt")
-			hud.update_score_player(player.health)
+			#sprite.play("Hurt")
+			HUD.update_score_player(player.health)
 			if player.health <= 0 :
 				if not player.death: 
 					player.Death()
@@ -126,26 +104,23 @@ func Death ():
 	#await get_tree().create_timer(0.3).timeout
 	#$Hurt_sound.play()
 	await get_tree().create_timer(1).timeout
-	$AnimatedSprite2D.play("Dead")
-	$PlayerDetectionarea/CollisionShape2D.set_deferred("disabled", true)
-	$hurtbox/CollisionShape2D.set_deferred("disabled", true)
-	
-	$CollisionShape2D.set_deferred("disabled", true)
+	sprite.play("Dead")
+	PlayerDetectionarea_CollisionShape.set_deferred("disabled", true)
+	hurtbox_CollisionShape.set_deferred("disabled", true)
+	CollisionShape.set_deferred("disabled", true)
 	await get_tree().create_timer(10).timeout
-	hud.show_game_over2()
+	HUD.show_game_over2()
 
 
 func _on_player_detectionarea_body_entered(body: Node2D) -> void:
-	#print(body.name)
-	#print(body.get_groups())
 	if body.is_in_group("player"):
 		player = body
 
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
-	#$AnimatedSprite2D.play("Hurt")
+	#sprite.play("Hurt")
 	health -= 10
-	hud.update_score_enemy(health)
+	HUD.update_score_enemy(health)
 	if health <= 0 :
 		if not death: 
 			Death()
@@ -154,10 +129,15 @@ func start_attack():
 	Attack = true
 	
 	current_attack = attackList.pick_random()
-	$AnimatedSprite2D.play(current_attack)
-	current_collisionShape = get_node(current_attack + "_hitbox/CollisionShape2D")
-	current_hitbox = get_node(current_attack + "_hitbox")
+	sprite.play(current_attack)
+	match current_attack:
+		"Attack1":
+			current_collisionShape = Attack1_hitbox_CollisionShape
+			current_hitbox = Attack1_hitbox
+		"Attack2":
+			current_collisionShape = Attack2_hitbox_CollisionShape
+			current_hitbox = Attack2_hitbox
+		"Attack3":
+			current_collisionShape = Attack3_hitbox_CollisionShape
+			current_hitbox = Attack3_hitbox
 	current_collisionShape.set_deferred("disabled", false)
-	print(current_collisionShape)
-	print(current_hitbox)
-	print(current_attack + "_hitbox")
